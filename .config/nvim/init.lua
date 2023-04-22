d16e22efc999d57830dc670ae3108d663dd40166
fracturedprism <71739935+fracturedprism@users.noreply.github.com>
2023-04-22 21:29:37 +0800

    update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -126,7 +126,7 @@ require ("lazy").setup({
 		name = 'rose-pine',
 		config = function()
 			require('rose-pine').setup{
-				dark_variant = 'moon'
+				-- dark_variant = 'moon'
 			}
 			vim.cmd('colo rose-pine')
 		end,
